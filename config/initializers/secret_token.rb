60ac39beca9b5b7c05b8b7d6f13317231b38a015
Jeremy Baker <jhubert@gmail.com>
2014-01-24 23:50:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OhMyCall::Application.config.secret_key_base = 'c1f7e954478e0d5cbcbfb885031daa6dacfc4e728885db9da4ec414825bfb22c9d12a34e2529723de5a1a8d40714b45bd4d36e76049ac6624d00d0bd745e3dfe'
