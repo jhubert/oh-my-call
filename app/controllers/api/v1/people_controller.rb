f49bb8fd3a3f9ced3bd8438e9fd71c3d5afd84ec
Jeremy Baker <jhubert@gmail.com>
2014-01-26 20:51:52 -0800

    First pass at real tests for the people controller

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -6,22 +6,32 @@
 # Routes tests are handled in test/integration/routes_test.rb
 class Api::V1::PeopleController < Api::V1::BaseController
   def index
-    render json: {}
+    render json: Person.all.to_json
   end
 
   def show
-    render json: {}
+    render json: Person.find(params[:id]).to_json
   end
 
   def create
+    Person.create(params.permit([
+      :phone_number, :fullname, :nickname, :active
+    ]))
+
     render json: {}
   end
 
   def update
+    Person.find(params[:id]).update_attributes(params.permit([
+      :phone_number, :fullname, :nickname, :active
+    ]))
+
     render json: {}
   end
 
   def destroy
+    Person.destroy(params[:id])
+
     head :no_content
   end
 end
--- test/controllers/api/v1/people_controller_test.rb
+++ test/controllers/api/v1/people_controller_test.rb
@@ -6,6 +6,14 @@ class Api::V1::PeopleController::IndexTest < ActionController::TestCase
   def setup
     @action_name = :index
   end
+
+  test 'returns an array of people' do
+    api_login_user
+    perform_request
+
+    assert Array, JSON.parse(response.body).class
+    assert response.body.include? Person.first.to_json
+  end
 end
 
 class Api::V1::PeopleController::ShowTest < ActionController::TestCase
@@ -13,7 +21,14 @@ class Api::V1::PeopleController::ShowTest < ActionController::TestCase
 
   def setup
     @action_name = :show
-    @base_params = { id: 1 }
+    @base_params = { id: people(:one).id }
+  end
+
+  test 'returns the details of the correct person' do
+    api_login_user
+    perform_request
+
+    assert_equal people(:one).to_json, response.body
   end
 end
 
@@ -24,6 +39,14 @@ class Api::V1::PeopleController::CreateTest < ActionController::TestCase
     @method = :post
     @action_name = :create
   end
+
+  test 'creates a new person' do
+    api_login_user
+
+    assert_difference 'Person.count' do
+      perform_request
+    end
+  end
 end
 
 class Api::V1::PeopleController::UpdateTest < ActionController::TestCase
@@ -32,7 +55,17 @@ class Api::V1::PeopleController::UpdateTest < ActionController::TestCase
   def setup
     @method = :put
     @action_name = :update
-    @base_params = { id: 1 }
+    @base_params = { id: people(:one).id }
+  end
+
+  test 'updates an existing person' do
+    api_login_user
+
+    @base_params = { id: people(:one).id, phone_number: '5555551234' }
+
+    perform_request
+
+    assert_equal '5555551234', Person.find(people(:one).id).phone_number
   end
 end
 
@@ -42,7 +75,15 @@ class Api::V1::PeopleController::DestroyTest < ActionController::TestCase
   def setup
     @method = :delete
     @action_name = :destroy
-    @base_params = { id: 1 }
+    @base_params = { id: people(:one).id }
     @success_response = :no_content
+  end
+
+  test 'removes an existing person' do
+    api_login_user
+
+    assert_difference 'Person.count', -1 do
+      perform_request
+    end
   end
 end
